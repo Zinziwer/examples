9c9dd4fc260bdaee76542ae8c1b7dfdd7c83fd92
Zinziwer <unislon@mail.ru>
2022-09-16 15:49:27 +0300

    MashaSara

---
+++ Masha/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("enter your name   ");
+string username = Console.ReadLine();
+if (username.ToLower() == "sara")
+    {
+        Console.WriteLine("Sara, glad to see you");
+        }
+    else
+    {
+        Console.Write("Hellow, ");
+        Console.Write(username);     
+    }
